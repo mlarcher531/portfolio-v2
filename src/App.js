64558483b75d0ecfb741461f42cdf54160020d78
mlarcher531 <mlarcher531@gmail.com>
2022-03-01 17:56:56 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div>
+  <div className="border border-red-500">Hello, world</div>
+</div>
+  );
+}
+
+export default App;
